82101980b6923b0e7e32eeae7b87361c00e9d77e
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-10 14:45:08 +0530

    css

---
+++ src/Compoent/Pages/ProductPage/ProductPage.js
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import style from "./ProductPage.module.css"
+import logo from "../../Images/logo.jpg"
+import searchicon from "../../Images/search.png"
+import shoppingCart from "../../Images/shopping-basket.png"
+import avatarimg from "../../Images/user.png"
+import menuicon from "../../Images/menu.png"
+import AnchorTemporaryDrawer from '../../AnchorTemporaryDrawer/AnchorTemporaryDrawer';
+import Footer from '../../Sections/Footer/Footer';
+import ProductSectionFirst from '../../ProductPageSection/SectionFirst/ProductSectionFirst';
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ProductPage(props) {
+  return (
+    <React.Fragment>
+             <AnchorTemporaryDrawer />
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar style={{backgroundColor:"white",boxShadow:"none"}}>
+          <Toolbar>
+              <div className={style.tool_bar}>
+                 <div className={style.left_section}>
+                    <p className="futura-pt-book">HOME</p>
+                    <p className="futura-pt-book">BULKORDER</p>
+                    <p className="futura-pt-book"> TRACKORDER</p>
+                 </div>
+                 <div  className={style.middle_section}>
+                 <div className={style.img_box}>
+                     <img src={logo} alt='logo' />
+                 </div>
+                 </div>
+         
+                 <div className={style.right_section}>
+                    <div className={style.icon_box}>
+                        <img  src={searchicon}  alt='search'/>
+                    </div>
+                    <div className={style.icon_box}>
+                        <img  src={avatarimg}  alt='user'/>
+                    </div>
+                    <div className={style.icon_box}>
+                        <img  src={shoppingCart}  alt='shopping'/>
+                    </div>
+                    <div className={style.icon_box4}>
+                        <img  src={menuicon}  alt='menu'/>
+                    </div>
+                 </div>
+              </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+      <div  className={style.Container}>
+           <ProductSectionFirst />
+          <Footer />
+        </div>
+    </React.Fragment>
+  );
+}
---
+++ src/Compoent/ProductPageSection/SectionFirst/ProductSectionFirst.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import style from "./ProductSectionFirst.module.css";
+import backgroundImage from "../../Images/banner-wine-festival-glass-wine-distillery-old-wooden-table-with-bunches.jpg";
+
+function ProductSectionFirst() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={style.main}>
+      <div
+        className={style.background}
+        style={{ transform: `translateY(-${scrollPosition * 0.5}px)` }} // Adjust the speed here
+      >
+        <img src={backgroundImage} alt="bg" className={style.bgImage} />
+      </div>
+      <div
+        className={style.content}
+        style={{ transform: `translateY(-${scrollPosition * 0.2}px)` }} // Adjust the speed here
+      >
+        <div className={style.description_box}>
+          <h2>PRODUCT  SINGLE  PAGE</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductSectionFirst;
