842b5f52b74bf2d85d64e8d884a5654dab2c547c
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-10 13:48:04 +0530

    css

--- src/Compoent/Sections/SectionThird/SectionThird.js
+++ src/Compoent/Sections/SectionThird/SectionThird.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./SectionThird.module.css";
 import textfile from "../../Images/text.png";
 import backgroundImage from "../../Images/banner-wine-festival-glass-wine-distillery-old-wooden-table-with-bunches.jpg";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function SectionThird() {
-  const alignCenter = { display: 'flex', alignItems: 'center' }
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className={style.main}>
-      <Parallax pages={1.5} className={style.Parallax} style={{ ...alignCenter, justifyContent: 'center' }}>
-        <ParallaxLayer offset={0.5} speed={0.8} style={{ ...alignCenter, justifyContent: 'center' }} >
-          <div className={style.background}>
-            <img src={backgroundImage} alt="bg" className={style.bgImage} />
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.5} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
-          <div className={style.content}>
-            <div className={style.description_box}>
-              <img src={textfile} alt="text" />
-              <h2>E L E G A N T &amp; U N I Q U E</h2>
-            </div>
-            <button className={style.btn}>DISCOVER →</button>
-          </div>
-        </ParallaxLayer>
-      </Parallax>
+      <div
+        className={style.background}
+        style={{ transform: `translateY(-${scrollPosition * 0.5}px)` }} // Adjust the speed here
+      >
+        <img src={backgroundImage} alt="bg" className={style.bgImage} />
+      </div>
+      <div
+        className={style.content}
+        style={{ transform: `translateY(-${scrollPosition * 0.2}px)` }} // Adjust the speed here
+      >
+        <div className={style.description_box}>
+          <img src={textfile} alt="text" />
+          <h2>E L E G A N T &amp; U N I Q U E</h2>
+        </div>
+        <button className={style.btn}>DISCOVER →</button>
+      </div>
     </div>
   );
 }
