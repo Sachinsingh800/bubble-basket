1daf3b53b1607dd4fea68d02f0b8fe9f7d4472ed
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-11 13:20:44 +0530

    css

--- src/Compoent/NavBar/NavBar.js
+++ src/Compoent/NavBar/NavBar.js
@@ -1,32 +1,32 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
-import style from "./NavBar.module.css"
-import logo from "../Images/logo.jpg"
-import searchicon from "../Images/search.png"
-import shoppingCart from "../Images/shopping-basket.png"
-import avatarimg from "../Images/user.png"
-import menuicon from "../Images/menu.png"
-import HomePageCarousel from '../HomePageCarousel/HomePageCarousel';
-import FirstSection from '../Sections/FirstSection/FirstSection';
-import AnchorTemporaryDrawer from '../AnchorTemporaryDrawer/AnchorTemporaryDrawer';
-import SecondSection from '../Sections/SecondSection/SecondSection';
-import SectionThird from '../Sections/SectionThird/SectionThird';
-import SectionFourth from '../Sections/SectionFourth/SectionFourth';
-import SectionFifth from '../Sections/SectionFifth/SectionFifth';
-import SectionSixth from '../Sections/SectionSixth/SectionSixth';
-import SectionSeventh from '../Sections/SectionSeventh/SectionSeventh';
-import SectionEight from '../Sections/SectionEight/SectionEight';
-import SectionNinth from '../Sections/SectionNinth/SectionNinth';
-import SectionTenth from '../Sections/SectionTenth/SectionTenth';
-import Footer from '../Sections/Footer/Footer';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Slide from "@mui/material/Slide";
+import style from "./NavBar.module.css";
+import logo from "../Images/logo.jpg";
+import searchicon from "../Images/search.png";
+import shoppingCart from "../Images/shopping-basket.png";
+import avatarimg from "../Images/user.png";
+import menuicon from "../Images/menu.png";
+import HomePageCarousel from "../HomePageCarousel/HomePageCarousel";
+import FirstSection from "../Sections/FirstSection/FirstSection";
+import AnchorTemporaryDrawer from "../AnchorTemporaryDrawer/AnchorTemporaryDrawer";
+import SecondSection from "../Sections/SecondSection/SecondSection";
+import SectionThird from "../Sections/SectionThird/SectionThird";
+import SectionFourth from "../Sections/SectionFourth/SectionFourth";
+import SectionFifth from "../Sections/SectionFifth/SectionFifth";
+import SectionSixth from "../Sections/SectionSixth/SectionSixth";
+import SectionSeventh from "../Sections/SectionSeventh/SectionSeventh";
+import SectionEight from "../Sections/SectionEight/SectionEight";
+import SectionNinth from "../Sections/SectionNinth/SectionNinth";
+import SectionTenth from "../Sections/SectionTenth/SectionTenth";
+import Footer from "../Sections/Footer/Footer";
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -56,55 +56,61 @@ HideOnScroll.propTypes = {
 export default function HideAppBar(props) {
   return (
     <React.Fragment>
-             <AnchorTemporaryDrawer />
+      <AnchorTemporaryDrawer />
       <CssBaseline />
       <HideOnScroll {...props}>
-        <AppBar style={{backgroundColor:"white",boxShadow:"none"}}>
+        <AppBar style={{ backgroundColor: "white", boxShadow: "none" }}>
           <Toolbar>
-              <div className={style.tool_bar}>
-                 <div className={style.left_section}>
-                    <p className="futura-pt-book">HOME</p>
-                    <p className="futura-pt-book">BULKORDER</p>
-                    <p className="futura-pt-book"> TRACKORDER</p>
-                 </div>
-                 <div  className={style.middle_section}>
-                 <div className={style.img_box}>
-                     <img src={logo} alt='logo' />
-                 </div>
-                 </div>
-         
-                 <div className={style.right_section}>
-                    <div className={style.icon_box}>
-                        <img  src={searchicon}  alt='search'/>
-                    </div>
-                    <div className={style.icon_box}>
-                        <img  src={avatarimg}  alt='user'/>
-                    </div>
-                    <div className={style.icon_box}>
-                        <img  src={shoppingCart}  alt='shopping'/>
-                    </div>
-                    <div className={style.icon_box4}>
-                        <img  src={menuicon}  alt='menu'/>
-                    </div>
-                 </div>
+            <div className={style.tool_bar}>
+              <ul className={style.left_section}>
+                <li>
+                  <a>HOME</a>
+                </li>
+                <li>
+                  <a>BULKORDER</a>
+                </li>
+                <li>
+                  <a>TRACKORDER</a>
+                </li>
+              </ul>
+              <div className={style.middle_section}>
+                <div className={style.img_box}>
+                  <img src={logo} alt="logo" />
+                </div>
               </div>
+
+              <div className={style.right_section}>
+                <div className={style.icon_box}>
+                  <img src={searchicon} alt="search" />
+                </div>
+                <div className={style.icon_box}>
+                  <img src={avatarimg} alt="user" />
+                </div>
+                <div className={style.icon_box}>
+                  <img src={shoppingCart} alt="shopping" />
+                </div>
+                <div className={style.icon_box4}>
+                  <img src={menuicon} alt="menu" />
+                </div>
+              </div>
+            </div>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
       <Toolbar />
-      <div  className={style.Container}>
-          <FirstSection/>
-          <SecondSection />
-          <SectionThird />
-          <SectionFourth />
-          <SectionFifth />
-          <SectionSixth />
-          <SectionSeventh />
-          <SectionEight />
-          <SectionNinth />
-          <SectionTenth />
-          <Footer />
-        </div>
+      <div className={style.Container}>
+        <FirstSection />
+        <SecondSection />
+        <SectionThird />
+        <SectionFourth />
+        <SectionFifth />
+        <SectionSixth />
+        <SectionSeventh />
+        <SectionEight />
+        <SectionNinth />
+        <SectionTenth />
+        <Footer />
+      </div>
     </React.Fragment>
   );
 }
