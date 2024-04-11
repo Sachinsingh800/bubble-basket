62b0e18da94fcb79fc2558c547cff6b10bf2222e
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-12 02:51:29 +0530

    css

--- src/Compoent/Sections/SectionFourth/SectionFourth.js
+++ src/Compoent/Sections/SectionFourth/SectionFourth.js
@@ -4,6 +4,9 @@ import product1 from "../../Images/26 pc.png";
 import product2 from "../../Images/Moet & Chandon Imperial Brut Champagne With 8pc 1.png";
 import product3 from "../../Images/dom perignon lady gaga rose.png";
 import textfile from "../../Images/text2.png";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function SectionFourth() {
   const collectionData = [
@@ -21,6 +24,20 @@ function SectionFourth() {
     },
   ];
 
+  const handleMouseEnter = (index) => {
+    const optionsBox = document.getElementById(`optionsBox_${index}`);
+    if (optionsBox) {
+      optionsBox.style.opacity = 1;
+    }
+  };
+
+  const handleMouseLeave = (index) => {
+    const optionsBox = document.getElementById(`optionsBox_${index}`);
+    if (optionsBox) {
+      optionsBox.style.opacity = 0;
+    }
+  };
+
   return (
     <div className={style.main}>
       <div className={style.heading_box}>
@@ -28,13 +45,29 @@ function SectionFourth() {
       </div>
       <div className={style.card_box}>
         {collectionData.map((item, index) => (
-          <div key={item.id} className={style.inner_container}>
+          <div
+            key={item.id}
+            className={style.inner_container}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+          >
             <div className={style.img_box}>
               <img src={item.img} alt={item.title} />
             </div>
             <div className={style.text_box}>
               <h5>{item.title}</h5>
               <p>{item.text}</p>
+            </div>
+            <div
+              className={style.optionsBox}
+              id={`optionsBox_${index}`}
+              style={{ opacity: 0 }}
+            >
+              <div className={style.options}>
+                <button className={style.optionButton1}><ShoppingCartIcon /></button>
+                <button className={style.optionButton2}><FavoriteBorderIcon/></button>
+                <button className={style.optionButton3}><VisibilityIcon  /></button>
+              </div>
             </div>
           </div>
         ))}
