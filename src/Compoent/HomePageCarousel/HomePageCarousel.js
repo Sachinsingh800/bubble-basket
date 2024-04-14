fa9d51392164f0f5ae7ac6d117923b79efd65ba1
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-14 16:07:13 +0530

    css

--- src/Compoent/HomePageCarousel/HomePageCarousel.js
+++ src/Compoent/HomePageCarousel/HomePageCarousel.js
@@ -1,29 +1,64 @@
 // HomePageCarousel.js
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { Button, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import style from "./HomePageCarousel.module.css";
-import { SlidingAnimation, SlidingAnimation2, SlidingAnimation3 } from '../SlidingAnimation/SlidingAnimation';
+import {
+  SlidingAnimation,
+  SlidingAnimation2,
+  SlidingAnimation3,
+} from "../SlidingAnimation/SlidingAnimation";
 
 function HomePageCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  const onPrevClick = () => {
+    if (index > 0) {
+      setIndex(index - 1);
+    } else if (index === 0) setIndex(2);
+  };
+  const onNextClick = () => {
+    if (index === 2) {
+      setIndex(0);
+    } else if (index === 0 || index > 0) setIndex(index + 1);
+  };
+
   return (
-    <Carousel slide={true} interval={6000}>
-      <Carousel.Item>
-        <div className={style.carousel_box}>
-          <SlidingAnimation />
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className={style.carousel_box}>
-          <SlidingAnimation2 />
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className={style.carousel_box}>
-          <SlidingAnimation3 />
-        </div>
-      </Carousel.Item>
-    </Carousel>
+    <>
+      <div className={style.button_container}>
+        <button onClick={onPrevClick}>Previous</button>
+        <button onClick={onNextClick}>Next</button>
+      </div>
+      <Carousel
+        slide={true}
+        interval={6000}
+        onSelect={handleSelect}
+        indicators={false}
+        controls={false}
+        activeIndex={index}
+      >
+        <Carousel.Item>
+          <div className={style.carousel_box}>
+            <SlidingAnimation />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={style.carousel_box}>
+            <SlidingAnimation2 />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={style.carousel_box}>
+            <SlidingAnimation3 />
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </>
   );
 }
 
