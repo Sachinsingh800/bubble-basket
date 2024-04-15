630e4b44a2da11930e2be5c54db41ffaf82360d1
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-16 02:31:04 +0530

    css

--- src/Compoent/CartPageSection/CartPageSectionSecond/CartPageSectionSecond.js
+++ src/Compoent/CartPageSection/CartPageSectionSecond/CartPageSectionSecond.js
@@ -42,68 +42,90 @@ function CartPageSectionSecond() {
     setData(updatedData);
   };
 
+  const calculateTotal = () => {
+    let total = 0;
+    data.forEach((item) => {
+      total += parseFloat(item.subTotal.slice(2));
+    });
+    return total.toFixed(2);
+  };
+
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <p className={style.product}>PRODUCT</p>
-        <p>PRICE</p>
-        <p>QUANTITY</p>
-        <p>SUBTOTAL</p>
-      </div>
-      {data.map((item, index) => (
-        <div key={index} className={style.container}>
-          <div className={style.first_box}>
-            <span
-              className={style.del_button}
-              onClick={() => handleRemoveProduct(index)}
-            >
-              x
-            </span>
-            <div className={style.img_box}>
-              <img src={item.productImg} alt={item.productName} />
+      {data.length > 0  &&
+           <div className={style.header}>
+           <p className={style.product}>PRODUCT</p>
+           <p>PRICE</p>
+           <p>QUANTITY</p>
+           <p>SUBTOTAL</p>
+         </div>
+      }
+ 
+      {data.length === 0 ? (
+        <div className={style.empty_cart}>
+           <p>YOUR CART IS CURRENTLY EMPTY.</p>
+           <button>RETURN TO SHOP →</button>
+        </div>
+       
+      ) : (
+        data.map((item, index) => (
+          <div key={index} className={style.container}>
+            <div className={style.first_box}>
+              <span
+                className={style.del_button}
+                onClick={() => handleRemoveProduct(index)}
+              >
+                x
+              </span>
+              <div className={style.img_box}>
+                <img src={item.productImg} alt={item.productName} />
+              </div>
+              {item.productName}
             </div>
-            {item.productName}
+
+            <div className={style.para}>{item.price}</div>
+
+            <div>
+              <input
+                className={style.quantity_box}
+                value={item.quantity}
+                type="number"
+                onChange={(e) =>
+                  handleQuantityChange(index, parseInt(e.target.value))
+                }
+              />
+            </div>
+
+            <div className={style.para}>{item.subTotal}</div>
           </div>
-
-          <div className={style.para}>{item.price}</div>
-
-          <div>
-            <input
-              className={style.quantity_box}
-              value={item.quantity}
-              type="number"
-              onChange={(e) =>
-                handleQuantityChange(index, parseInt(e.target.value))
-              }
-            />
+        ))
+      )}
+      {data.length > 0 && (
+        <>
+          <div className={style.buttons_box}>
+            <div>
+              <button>Coupon Code</button>
+              <button>APPLY COUPON → </button>
+            </div>
+            <button>UPDATE CART → </button>
           </div>
-
-          <div className={style.para}>{item.subTotal}</div>
-        </div>
-      ))}
-      <div className={style.buttons_box}>
-        <div>
-          <button>Coupon Code</button>
-          <button>APPLY COUPON → </button>
-        </div>
-        <button>UPDATE CART → </button>
-      </div>
-      <div className={style.cart_container}>
-        <h6>CART TOTALS</h6>
-        <div className={style.cart_box}>
-          <div className={style.first_box}>SUBTOTAL</div>
-          <div>$ 377.00</div>
-        </div>
-        <div className={style.cart_box}>
-          <div className={style.first_box}>TOTAL</div>
-          <div className={style.total_price}>$ 377.00</div>
-        </div>
-        
-      </div>
-      <br/>
-      <div className={style.buttons_box2}>
-      <button>PROCEED  TO CHECKOUT → </button>
-      </div>
+          <div className={style.cart_container}>
+            <h6>CART TOTALS</h6>
+            <div className={style.cart_box}>
+              <div className={style.first_box}>SUBTOTAL</div>
+              <div>$ {calculateTotal()}</div>
+            </div>
+            <div className={style.cart_box}>
+              <div className={style.first_box}>TOTAL</div>
+              <div className={style.total_price}>$ {calculateTotal()}</div>
+            </div>
+          </div>
+          <br />
+          <div className={style.buttons_box2}>
+            <button>PROCEED TO CHECKOUT → </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
