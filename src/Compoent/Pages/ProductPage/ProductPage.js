a7d4537cc22af28d0b4c86d28aabe32e55b831a0
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-16 13:22:54 +0530

    css

--- src/Compoent/Pages/ProductPage/ProductPage.js
+++ src/Compoent/Pages/ProductPage/ProductPage.js
@@ -53,7 +53,7 @@ export default function ProductPage(props) {
   };
   return (
     <React.Fragment>
-      <AnchorTemporaryDrawer />
+         <span className={style.dawer}><AnchorTemporaryDrawer /></span> 
       <CssBaseline />
       <HideOnScroll {...props}>
         <AppBar style={{ backgroundColor: "white", boxShadow: "none" }}>
