63b8114db3710781855f02b7e9e01a6a7ef081f1
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-18 14:37:50 +0530

    css

--- src/Compoent/BlogPageSection/BlogPageSecondSection/BlogPageSecondSection.js
+++ src/Compoent/BlogPageSection/BlogPageSecondSection/BlogPageSecondSection.js
@@ -2,6 +2,10 @@ import React from "react";
 import style from "./BlogPageSecondSection.module.css";
 import blog1 from "../../Images/banner-wine-festival-glass-wine-distillery-old-wooden-table-with-bunches.jpg";
 import blog2 from "../../Images/glass-wine-old-table-with-vineyard-background.jpg";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function BlogPageSecondSection() {
   const blogData = [
@@ -33,20 +37,30 @@ function BlogPageSecondSection() {
           <div className={style.blog_img_box}>
             <img src={item.img} alt="blog" />
           </div>
-          <div>
-            <p>{item.author}</p> -<p>{item.date}</p>
+          <br />
+          <div className={style.author_box}>
+            <p>{item.author}</p>
+            <p>-</p> <p>{item.date}</p>
           </div>
           <div>
-            <h1>{item.title}</h1>
+            <h6>{item.title}</h6>
             <p>{item.content}</p>
           </div>
-          <div>
+          <div className={style.bottom_box}>
             <p>READ MORE →</p>
             <ul>
-              <li>f</li>
-              <li>in</li>
-              <li>in</li>
-              <li>in</li>
+              <li>
+                <FacebookRoundedIcon className={style.icon} />
+              </li>
+              <li>
+                <InstagramIcon  className={style.icon}/>
+              </li>
+              <li>
+                <LinkedInIcon className={style.icon} />
+              </li>
+              <li>
+                <TwitterIcon className={style.icon} />
+              </li>
             </ul>
           </div>
         </div>
