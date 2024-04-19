b49dee6f9f13ba13af3e08e813d72b64a7230e46
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-19 17:08:09 +0530

    css

--- src/Compoent/ContactUsPageSection/ContactUsPageSectionSecond/ContactUsPageSectionSecond.js
+++ src/Compoent/ContactUsPageSection/ContactUsPageSectionSecond/ContactUsPageSectionSecond.js
@@ -46,10 +46,9 @@ function ContactUsPageSectionSecond() {
         </form>
         <div className={style.map_box}>
           <iframe 
+          className={style.map}
             title="Google Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.6537595606677!2d-77.26227708428358!3d38.90058757957322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b64d04c7f8daa7%3A0x1aa1e043c36b5f3!2s501%20Church%20St%20NE%2C%20Vienna%2C%20VA%2022180%2C%20USA!5e0!3m2!1sen!2suk!4v1648740255627!5m2!1sen!2suk" 
-            width="600" 
-            height="500" 
             allowfullscreen=""
             loading="lazy"
           ></iframe>
