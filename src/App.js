635e8ed3ad11f5c5f443149a78d4241fcdedf199
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-20 18:30:56 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css'; // Import module-level CSS
 import HideAppBar from './Compoent/NavBar/NavBar';
 
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleClose = () => {
+    setShowPopup(false);
+  };
+
+  const handleNo = () => {
+    window.close(); // Close the tab
+  };
+
   return (
-    <div className="App">
-     <HideAppBar />
+    <div className={styles.App}> {/* Use module-level CSS class */}
+      <HideAppBar />
+      {showPopup && (
+        <div className={styles.popup}> {/* Use module-level CSS class */}
+          <div className={styles.popup_content}> {/* Use module-level CSS class */}
+            <h2>Age Verification</h2>
+            <p>ARE YOU OVER 18?</p>
+            <p>By entering this site you agree to our Privacy Policy</p>
+            <p>This website requires you to be 18 years of age or older to access it. Please verify your age to view the content.</p>
+            <div className={styles.button_container}> {/* Use module-level CSS class */}
+              <button onClick={handleClose}>Yes, I am</button>
+              <button onClick={handleNo}>No, I am not</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
