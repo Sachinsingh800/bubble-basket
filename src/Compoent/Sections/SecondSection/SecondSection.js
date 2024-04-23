3e7536f9ed220fcd485d1b45019e20c6e94d2b64
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-04-23 17:00:01 +0530

    css

--- src/Compoent/Sections/SecondSection/SecondSection.js
+++ src/Compoent/Sections/SecondSection/SecondSection.js
@@ -1,32 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./SecondSection.module.css";
 import product1 from "../../Images/26 pc.png";
 import product2 from "../../Images/Moet & Chandon Imperial Brut Champagne With 8pc 1.png";
 import product3 from "../../Images/dom perignon lady gaga rose.png";
-import AddIcon from '@mui/icons-material/Add';
-import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
+import { nanoid } from "nanoid";
+import { updateCart } from "../../Recoil/Recoil";
+import { useRecoilState } from "recoil";
 
 function SecondSection() {
   const [openPopoverId, setOpenPopoverId] = useState(null);
+  const [update, setUpdate] = useRecoilState(updateCart);
+  const [showTick, setShowTick] = useState(null);
 
   const collectionData = [
     {
-      id: 1,
-      title: "WINE",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo voluptates iure eaque dolorum repudiand",
-      img: product3,
+      id: nanoid(),
+      productCategory: "HAND - PAINTED",
+      productName: "CHAMPAGNE",
+      productDescription:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo voluptates iure eaque dolorum repudiand",
+      productImg: product3,
+      productRating: 4,
+      price: 79.0,
+      quantity: 1,
+      subTotal: 79.0,
     },
     {
-      id: 2,
-      title: "CHAMPAGNE",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo voluptates iure eaque dolorum repudiand",
-      img: product2,
+      id: nanoid(),
+      productCategory: "PERSONALISED",
+      productName: "WINE",
+      productDescription:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo voluptates iure eaque dolorum repudiand",
+      productImg: product2,
+      productRating: 4,
+      price: 100.0,
+      quantity: 1,
+      subTotal: 100.0,
     },
     {
-      id: 3,
-      title: "CHOCOLATE",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo voluptates iure eaque dolorum repudiand",
-      img: product1,
+      id: nanoid(),
+      productCategory: "PERSONALISED",
+      productName: "CHOCOLATE",
+      productDescription:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quo voluptates iure eaque dolorum repudiand",
+      productImg: product1,
+      productRating: 4,
+      price: 100.0,
+      quantity: 1,
+      subTotal: 100.0,
     },
   ];
 
@@ -36,6 +59,13 @@ function SecondSection() {
 
   const handleClosePopover = () => {
     setOpenPopoverId(null);
+  };
+
+  const handleAddToCart = (item) => {
+    const cartData = JSON.parse(localStorage.getItem("cartData")) || [];
+    localStorage.setItem("cartData", JSON.stringify([...cartData, item]));
+    setUpdate(update + 1);
+    setShowTick(item.id);
   };
 
   return (
@@ -51,50 +81,26 @@ function SecondSection() {
       <div className={style.card_box}>
         {collectionData.map((item) => (
           <div key={item.id} className={style.inner_container}>
-            {openPopoverId === item.id ? (
-              <button onClick={handleClosePopover} className={style.addBtn}>
-                <CloseIcon/>
-              </button>
-            ) : (
-              <button
-                onClick={() => handleOpenPopover(item.id)}
-                className={style.addBtn}
-              >
-            <AddIcon/>
-              </button>
-            )}
-            {openPopoverId === item.id && (
-              <div className={style.popover}>
-                <button className={style.closeBtn} onClick={handleClosePopover}>
-              <CloseIcon/>
-                </button>
-                <div className={style.popover_container}>
-                  <div className={style.img_container}>
-                    <div className={style.popover_img_box}>
-                      <img src={item.img} alt={item.title} />
-                    </div>
-                  </div>
-                  <div className={style.des}>
-                    <h6>{item.title}</h6>
-                    <span>{item.text}</span>
-                    <div>
-                      <h6>$45.00/bottle</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
+            <button
+              onClick={() => handleAddToCart(item)}
+              className={style.addBtn}
+            >
+              {showTick === item.id ? "✓" : <AddIcon />}
+            </button>
+
             <div className={style.img_box}>
-              <img src={item.img} alt={item.title} />
+              <img src={item.productImg} alt={item.title} />
             </div>
             <div className={style.text_box}>
-              <h5>{item.title}</h5>
-              <p>{item.text}</p>
+              <h5>{item.productName}</h5>
+              <p>{item.productDescription}</p>
             </div>
           </div>
         ))}
       </div>
-     <a href="/ColumnPage"><button className={style.viewAllbtn}>VIEW ALL →</button></a> 
+      <a href="/ColumnPage">
+        <button className={style.viewAllbtn}>VIEW ALL →</button>
+      </a>
     </div>
   );
 }
