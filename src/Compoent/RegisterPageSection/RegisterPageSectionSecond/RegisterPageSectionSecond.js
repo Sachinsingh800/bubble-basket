778ddade561788331f18f42683f5a56662be55b9
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-05-04 17:23:01 +0530

    css

--- src/Compoent/RegisterPageSection/RegisterPageSectionSecond/RegisterPageSectionSecond.js
+++ src/Compoent/RegisterPageSection/RegisterPageSectionSecond/RegisterPageSectionSecond.js
@@ -80,88 +80,94 @@ function RegisterPageSectionSecond() {
 
   return (
     <div className={style.main}>
-      {showVerification && (
-        <div>
-          <input placeholder="otp" onChange={(e) => setOtp(e.target.value)} />
-          <button onClick={handleVerification}>verify Email </button>
+      {showVerification ? (
+        <div className={style.form}>
+          <div className={style.input_box}>
+            <h4>VERIFY YOUR EMAIL</h4>
+            <input type="number" placeholder="otp" onChange={(e) => setOtp(e.target.value)} />
+            <button onClick={handleVerification}>VERIFY EMAIL →</button>
+          </div>
         </div>
+      ) : (
+        <form onSubmit={handleSubmit} className={style.form}>
+          <h4>REGISTER</h4>
+          <div className={style.input_box}>
+            <label htmlFor="firstName">First Name *</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.input_box}>
+            <label htmlFor="lastName">Last Name *</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.input_box}>
+            <label htmlFor="email">Email *</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.input_box}>
+            <label htmlFor="telephone">Telephone *</label>
+            <PhoneInput
+              className={style.phone_box}
+              country="in" // Set the default country
+              value={formData.telephone}
+              onChange={(value) =>
+                setFormData({ ...formData, telephone: value })
+              }
+              inputProps={{
+                name: "telephone",
+                required: true,
+              }}
+            />
+          </div>
+          <div className={style.input_box}>
+            <label htmlFor="password">Password *</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.input_box}>
+            <label htmlFor="confirmPassword">Confirm Password *</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {passwordError && <p className={style.error}>{passwordError}</p>}
+          <button type="submit">REGISTER →</button>
+          <p>
+            Already have an account? <a href="/Login">Login</a>
+          </p>
+        </form>
       )}
-      <form onSubmit={handleSubmit} className={style.form}>
-        <h4>REGISTER</h4>
-        <div className={style.input_box}>
-          <label htmlFor="firstName">First Name *</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={style.input_box}>
-          <label htmlFor="lastName">Last Name *</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={style.input_box}>
-          <label htmlFor="email">Email *</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={style.input_box}>
-          <label htmlFor="telephone">Telephone *</label>
-          <PhoneInput
-            className={style.phone_box}
-            country="in" // Set the default country
-            value={formData.telephone}
-            onChange={(value) => setFormData({ ...formData, telephone: value })}
-            inputProps={{
-              name: "telephone",
-              required: true,
-            }}
-          />
-        </div>
-        <div className={style.input_box}>
-          <label htmlFor="password">Password *</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={style.input_box}>
-          <label htmlFor="confirmPassword">Confirm Password *</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {passwordError && <p className={style.error}>{passwordError}</p>}
-        <button type="submit">REGISTER →</button>
-        <p>
-          Already have an account? <a href="/Login">Login</a>
-        </p>
-      </form>
     </div>
   );
 }
