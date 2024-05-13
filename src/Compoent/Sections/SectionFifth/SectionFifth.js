a8fdce50b1dc312b058bc4f2b68b6007cb68c0c3
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-05-13 20:27:54 +0530

    css

--- src/Compoent/Sections/SectionFifth/SectionFifth.js
+++ src/Compoent/Sections/SectionFifth/SectionFifth.js
@@ -1,127 +1,124 @@
-import React from 'react'
-import style from "./SectionFifth.module.css"
-import drum from "../../Images/drum.png"
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
+import React, { useEffect, useState } from "react";
+import style from "./SectionFifth.module.css";
+import drum from "../../Images/drum.png";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function SectionFifth() {
-    const [age, setAge] = React.useState('');
+  const [category, setCategory] = useState("");
+  const [categoryData, setCategoryData] = useState([]);
 
-    const handleChange = (event) => {
-      setAge(event.target.value);
-    };
+  useEffect(() => {
+    const allcategory = JSON.parse(localStorage.getItem("all_category"));
+    setCategoryData(allcategory);
+  }, []);
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
+
   return (
     <div className={style.main}>
       <div className={style.img_box}>
-        <img src={drum} alt='Drum'/>
-        </div>
+        <img src={drum} alt="Drum" />
+      </div>
       <div className={style.outer_container}>
-       <div className={style.container}>
-         <div className={style.input_container}>
-         <p>SHOP BY CATEGORY</p>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 180,}}>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={age}
-          onChange={handleChange}
-          label="Age"
-          style={{color:"maroon" }}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-         </div>
-       
-     <h6>Usu ad illum petentium error feugait</h6>
+        <div className={style.container}>
+          <div className={style.input_container}>
+            <p>SHOP BY CATEGORY</p>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 180 }}>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={category}
+                onChange={handleChange}
+                label="category"
+                 style={{ color: "maroon", fontWeight: 700,fontFamily:"Jost" }}
+              >
+                {categoryData.map((item, index) => (
+                  <MenuItem key={index} value={item.categoryName}>
+                    {item.categoryName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <h6>Usu ad illum petentium error feugait</h6>
+        </div>
+        <div className={style.container}>
+          <div className={style.input_container}>
+            <p>GIFT BY BRAND</p>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 180 }}>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={category}
+                onChange={handleChange}
+                label="category"
+                 style={{ color: "maroon", fontWeight: 700,fontFamily:"Jost" }}
+              >
+                {categoryData.map((item, index) => (
+                  <MenuItem key={index} value={item.categoryName}>
+                    {item.categoryName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <h6>Magna harum probatus ex eam mea</h6>
+        </div>
+        <div className={style.container}>
+          <div className={style.input_container}>
+            <p>SHOP BY LUXURY</p>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 180 }}>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={category}
+                onChange={handleChange}
+                label="category"
+                style={{ color: "maroon", fontWeight: 700,fontFamily:"Jost" }}
+              >
+                {categoryData.map((item, index) => (
+                  <MenuItem key={index} value={item.categoryName}>
+                    {item.categoryName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <h6>Ne possit suavitate pri sint erroribus</h6>
+        </div>
+        <div className={style.container}>
+          <div className={style.input_container}>
+            <p>GIFT BY ORIGIN</p>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 180 }}>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={category}
+                onChange={handleChange}
+                label="Category"
+                 style={{ color: "maroon", fontWeight: 700,fontFamily:"Jost" }}
+              >
+                {categoryData.map((item, index) => (
+                  <MenuItem key={index} value={item.categoryName}>
+                    {item.categoryName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <h6>Dicant habemus definitionem sed ei elit</h6>
+        </div>
+      </div>
     </div>
-       <div className={style.container}>
-         <div className={style.input_container}>
-         <p>GIFT BY BRAND</p>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 180,}}>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={age}
-          onChange={handleChange}
-          label="Age"
-          style={{color:"maroon" }}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-         </div>
-       
-     <h6>Magna harum probatus ex eam mea
-</h6>
-    </div>
-       <div className={style.container}>
-         <div className={style.input_container}>
-         <p>SHOP BY LUXURY</p>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 180,}}>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={age}
-          onChange={handleChange}
-          label="Age"
-          style={{color:"maroon" }}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-         </div>
-       
-     <h6>Ne possit suavitate pri sint erroribus</h6>
-    </div>
-       <div className={style.container}>
-         <div className={style.input_container}>
-         <p>GIFT BY ORIGIN</p>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 180,}}>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={age}
-          onChange={handleChange}
-          label="Age"
-          style={{color:"maroon" }}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-         </div>
-       
-     <h6>Dicant habemus denitionem sed ei elit</h6>
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default SectionFifth
-
-
-
+export default SectionFifth;
