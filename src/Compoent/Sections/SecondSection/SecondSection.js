52ad46ab73d8d60654d693cb860486ae3df7a9a0
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-05-20 14:34:49 +0530

    css

--- src/Compoent/Sections/SecondSection/SecondSection.js
+++ src/Compoent/Sections/SecondSection/SecondSection.js
@@ -13,61 +13,9 @@ function SecondSection() {
   const [productData, setProductData] = useState([]);
 
   useEffect(() => {
-    getAllCategory()
-  }, []);
-
-  const getAllCategory = async () => {
-    // Function to retrieve token from cookies
-    // Function to retrieve token from cookies
-    function getToken() {
-      return document.cookie.replace(
-        /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
-        "$1"
-      );
-    }
-
-    // Retrieve token
-    const token = getToken();
-
-    try {
-      const headers = {
-        "x-auth-token": token, // Pass the token in the header
-        "Content-Type": "application/json", // Set content type to JSON
-      };
-      const response = await axios.get(
-        `https://www.backend.luxurybubblebasket.com/admin/category/getAll`,
-        {
-          headers,
-        }
-      );
-
-      const { status, message, data } = response.data;
-      if (status) {
-        console.log(data, "data aaa raha");
-
-        setProductData(data?.slice(0,3));
-      }
-
-      // Handle response data as needed
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        // Axios error (HTTP error)
-        const { response } = error;
-        // Set the error message
-        const errorMessage = response.data.message;
-        // alert(errorMessage);
-        // Log the error message as a string
-        localStorage.setItem("allAdress", JSON.stringify([]));
-        // alert(errorMessage);
-        console.error("Axios Error:", errorMessage);
-        // window.location.href = "/Login";
-      } else {
-        // Network error (e.g., no internet connection)
-        // alert("Something went wrong");
-        console.error("Network Error:", error.message);
-      }
-    }
-  };
+    const allCategory=JSON.parse(localStorage.getItem("all_category")) 
+    setProductData(allCategory?.slice(0,3));
+}, []);
 
 
   const handleAddToCartInBeckend = async (productId) => {
