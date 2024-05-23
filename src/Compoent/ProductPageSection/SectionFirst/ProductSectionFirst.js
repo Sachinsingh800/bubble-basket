1c9e5d4ae98ceebb8f6c9e63f92187f49ff93cd4
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-05-23 14:27:52 +0530

    css

--- src/Compoent/ProductPageSection/SectionFirst/ProductSectionFirst.js
+++ src/Compoent/ProductPageSection/SectionFirst/ProductSectionFirst.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import style from "./ProductSectionFirst.module.css";
 import backgroundImage from "../../Images/banner-wine-festival-glass-wine-distillery-old-wooden-table-with-bunches.jpg";
+import { getAllProduct } from "../../Apis/Apis";
+import { useParams } from "react-router-dom";
 
 function ProductSectionFirst() {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [productData, setProductData] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,7 +20,20 @@ function ProductSectionFirst() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+  useEffect(()=>{
+    handleProductData()
+  },[])
 
+  const handleProductData = async () => {
+    try {
+      const response = await getAllProduct();
+      // Show only the first three products
+      setProductData(response.data);
+    } catch (error) {
+      console.error("Error getting product data:", error);
+    }
+  };
+  const product = productData.find((item) => item._id.toString() === id);
   return (
     <div className={style.main}>
       <div
@@ -30,7 +47,7 @@ function ProductSectionFirst() {
         style={{ transform: `translateY(-${scrollPosition * 0.4}px)` }} // Adjust the speed here
       >
         <div className={style.description_box}>
-          <h2>PRODUCT  SINGLE  PAGE</h2>
+          <h2> {product?.category}</h2>
         </div>
       </div>
     </div>
