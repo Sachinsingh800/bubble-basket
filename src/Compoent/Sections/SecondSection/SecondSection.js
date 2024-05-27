a18c7d3159dba1ee41f711f815776a8324054346
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-05-27 13:41:21 +0530

    css

--- src/Compoent/Sections/SecondSection/SecondSection.js
+++ src/Compoent/Sections/SecondSection/SecondSection.js
@@ -3,7 +3,12 @@ import style from "./SecondSection.module.css";
 import AddIcon from "@mui/icons-material/Add";
 import { updateCart } from "../../Recoil/Recoil";
 import { useRecoilState } from "recoil";
-import { AddtoCart, getAllCategory, getAllProduct, getTop1Category } from "../../Apis/Apis";
+import {
+  AddtoCart,
+  getAllCategory,
+  getAllProduct,
+  getTop1Category,
+} from "../../Apis/Apis";
 import axios from "axios";
 
 function SecondSection() {
@@ -11,46 +16,38 @@ function SecondSection() {
   const [loading, setLoading] = useState(false);
   const [showTick, setShowTick] = useState(null);
   const [productData, setProductData] = useState([]);
-  const [top1Category,setTop1Category] = useState([])
-  const [top2Category,setTop2Category] = useState([])
-  const [top3Category,setTop3Category] = useState([])
+  const [top1Category, setTop1Category] = useState([]);
+  const [top2Category, setTop2Category] = useState([]);
+  const [top3Category, setTop3Category] = useState([]);
 
-
-  console.log(top1Category
-    ,"data top")
+  console.log(top1Category, "data top");
 
   useEffect(() => {
-    handleAllCategory()
-    handleParticularCategory()
-}, []);
+    handleAllCategory();
+    handleParticularCategory();
+  }, []);
 
-const handleAllCategory = async () => {
-  try {
-    const response = await getAllCategory();
-    setProductData(response?.data?.slice(0,3));
-  } catch (error) {
-    console.error("Error in handleAllCategory function:", error);
-  }
-};
-const handleParticularCategory = async () => {
-  try {
-    const response1 = await getTop1Category("top1");
-    const response2 = await getTop1Category("top2");
-    const response3 = await getTop1Category("top3");
-    console.log(response2,"and",response3)
-    setTop1Category(response1.data[0]);
-    setTop2Category(response2.data[0]);
-    setTop3Category(response3.data[0]);
-    
-  } catch (error) {
-    console.error("Error in handleAllCategory function:", error);
-  }
-};
-
-
-
-
-  
+  const handleAllCategory = async () => {
+    try {
+      const response = await getAllCategory();
+      setProductData(response?.data?.slice(0, 3));
+    } catch (error) {
+      console.error("Error in handleAllCategory function:", error);
+    }
+  };
+  const handleParticularCategory = async () => {
+    try {
+      const response1 = await getTop1Category("top1");
+      const response2 = await getTop1Category("top2");
+      const response3 = await getTop1Category("top3");
+      console.log(response2, "and", response3);
+      setTop1Category(response1.data[0]);
+      setTop2Category(response2.data[0]);
+      setTop3Category(response3.data[0]);
+    } catch (error) {
+      console.error("Error in handleAllCategory function:", error);
+    }
+  };
 
   return (
     <div className={style.main}>
@@ -64,73 +61,84 @@ const handleParticularCategory = async () => {
       </div>
       <div className={style.card_box}>
         {loading && <p>Loading...</p>}
-   
-          <div  className={style.inner_container}      onClick={() =>
+
+        <div
+          className={style.inner_container}
+          onClick={() =>
             (window.location.href = `/${top1Category?.categoryName}`)
-          }>
-            <button
-              // onClick={() => handleAddToCart(top1Category)}
-              className={style.addBtn}
-            >
-              {showTick === top1Category?._id ? "✓" : <AddIcon />}
-            </button>
+          }
+        >
+          <button
+            // onClick={() => handleAddToCart(top1Category)}
+            className={style.addBtn}
+          >
+            {showTick === top1Category?._id ? "✓" : <AddIcon />}
+          </button>
 
-            <div className={style.img_box}>
-              <img src={top1Category?.categoryImg?.url} alt={top1Category?.title} />
-            </div>
-            <div className={style.text_box}>
-              <h5>{top1Category?.categoryName}</h5>
-              <p>{top1Category?.content}</p>
-            </div>
+          <div className={style.img_box}>
+            <img
+              src={top1Category?.categoryImg?.url}
+              alt={top1Category?.title}
+            />
           </div>
+          <div className={style.text_box}>
+            <h5>{top1Category?.categoryName}</h5>
+            <p>{top1Category?.content}</p>
+          </div>
+        </div>
 
-
-
-          
-          <div  className={style.inner_container}      onClick={() =>
+        <div
+          className={style.inner_container}
+          onClick={() =>
             (window.location.href = `/${top2Category?.categoryName}`)
-          }>
-            <button
-              // onClick={() => handleAddToCart(top2Category)}
-              className={style.addBtn}
-            >
-              {showTick === top2Category?._id ? "✓" : <AddIcon />}
-            </button>
+          }
+        >
+          <button
+            // onClick={() => handleAddToCart(top2Category)}
+            className={style.addBtn}
+          >
+            {showTick === top2Category?._id ? "✓" : <AddIcon />}
+          </button>
 
-            <div className={style.img_box}>
-              <img src={top2Category?.categoryImg?.url} alt={top2Category?.title} />
-            </div>
-            <div className={style.text_box}>
-              <h5>{top2Category?.categoryName}</h5>
-              <p>{top2Category?.content}</p>
-            </div>
+          <div className={style.img_box}>
+            <img
+              src={top2Category?.categoryImg?.url}
+              alt={top2Category?.title}
+            />
           </div>
+          <div className={style.text_box}>
+            <h5>{top2Category?.categoryName}</h5>
+            <p>{top2Category?.content}</p>
+          </div>
+        </div>
 
-
-
-
-          <div  className={style.inner_container}      onClick={() =>
+        <div
+          className={style.inner_container}
+          onClick={() =>
             (window.location.href = `/${top3Category?.categoryName}`)
-          }>
-            <button
-              // onClick={() => handleAddToCart(top3Category)}
-              className={style.addBtn}
-            >
-              {showTick === top3Category?._id ? "✓" : <AddIcon />}
-            </button>
+          }
+        >
+          <button
+            // onClick={() => handleAddToCart(top3Category)}
+            className={style.addBtn}
+          >
+            {showTick === top3Category?._id ? "✓" : <AddIcon />}
+          </button>
 
-            <div className={style.img_box}>
-              <img src={top3Category?.categoryImg?.url} alt={top3Category?.title} />
-            </div>
-            <div className={style.text_box}>
-              <h5>{top3Category?.categoryName}</h5>
-              <p>{top3Category?.content}</p>
-            </div>
+          <div className={style.img_box}>
+            <img
+              src={top3Category?.categoryImg?.url}
+              alt={top3Category?.title}
+            />
           </div>
-
+          <div className={style.text_box}>
+            <h5>{top3Category?.categoryName}</h5>
+            <p>{top3Category?.content}</p>
+          </div>
+        </div>
       </div>
       <a href="/Product">
-        <button className={style.viewAllbtn}>VIEW ALL  ⟶</button>
+        <button className={style.viewAllbtn}>VIEW ALL ⟶</button>
       </a>
     </div>
   );
