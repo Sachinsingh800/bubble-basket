e64de35dfa6411d46856563f9761096d40301abd
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-06-04 14:28:18 +0530

    css

--- src/Compoent/ColumnPageSection/ColumnPageSectionFirst/ColumnPageSectionFirst.js
+++ src/Compoent/ColumnPageSection/ColumnPageSectionFirst/ColumnPageSectionFirst.js
@@ -1,49 +1,38 @@
-import React, { useState, useEffect } from "react";
-import style from "./ColumnPageSectionFirst.module.css";
-import backgroundImage from "../../Images/column page1.jpg";
-import backgroundImageMob from "../../Images/column page mob1.jpg";
-import { useParams } from "react-router-dom";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import styles from "./ColumnPageSectionFirst.module.css"; // Import styles
+import HomeIcon from '@mui/icons-material/Home';
 
 function ColumnPageSectionFirst() {
-  const [scrollPosition, setScrollPosition] = useState(0);
   const { category } = useParams();
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
+  console.log(category, "breadcrumbs");
 
-    window.addEventListener("scroll", handleScroll);
+  // Function to generate breadcrumb list items
+  const generateBreadcrumbs = () => {
+    const categories = category.split("/");
+    const breadcrumbs = [
+      { title: <HomeIcon />, url: "/" }, // Assuming your home route is "/"
+      { title: `/    ${categories[0]} GIFT BASKETS`, url: `/${categories[0]}` },
+      { title: `/    ${category}`,  url:""}, // Dynamic URL based on category
+    ];
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+    return breadcrumbs.map((crumb, index) => (
+      <li key={index} className={styles.breadcrumbItem}>
+        {index < breadcrumbs.length - 1 ? (
+          <Link to={crumb.url} className={styles.url}>
+            {crumb.title}
+          </Link>
+        ) : (
+          <span>{crumb.title}</span>
+        )}
+      </li>
+    ));
+  };
 
   return (
-    <div className={style.main}>
-      <div
-        className={style.background}
-        style={{ transform: `translateY(${scrollPosition * 0.2}px)` }} // Adjust the speed here
-      >
-        <img src={backgroundImage} alt="bg" className={style.bgImage} />
-        <img src={backgroundImageMob} alt="bg" className={style.bgImagemob} />
-      </div>
-      <div
-        className={style.content}
-        style={{ transform: `translateY(-${scrollPosition * 0.4}px)` }} // Adjust the speed here
-      >
-        <div className={style.description_box}>
-          <h2>{category}</h2>
-        </div>
-      </div>
-      
-      <div
-        className={style.contentmob}
-      >
-        <div className={style.description_box}>
-          <h2>{category}</h2>
-        </div>
-      </div>
+    <div className={styles.main}>
+      <h1>{category}</h1>
+      <ul className={styles.breadcrumbs}>{generateBreadcrumbs()}</ul>
     </div>
   );
 }
