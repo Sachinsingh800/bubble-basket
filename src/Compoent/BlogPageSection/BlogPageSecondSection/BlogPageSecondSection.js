7ea023478941d1e7803ba9ad9ae087ca44f48bb3
sachinsingh800 <sachinsinghgnc@gmail.com>
2024-06-07 21:27:18 +0530

    css

--- src/Compoent/BlogPageSection/BlogPageSecondSection/BlogPageSecondSection.js
+++ src/Compoent/BlogPageSection/BlogPageSecondSection/BlogPageSecondSection.js
@@ -37,60 +37,66 @@ function BlogPageSecondSection() {
   const formatTitleForUrl = (title) => {
     return title.replace(/\s+/g, "-").replace(/:/g, "");
   };
+
+  const generateHelmet = (item) => (
+    <Helmet>
+      <html lang="en" />
+      <meta charSet="utf-8" />
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <title>{item.blogTitle}</title>
+      <meta name="description" content={item.shortDescription} />
+      <link
+        rel="canonical"
+        href={`https://www.luxurybubblebasket.com/Blog/${formatTitleForUrl(
+          item.blogTitle
+        )}`}
+      />
+    </Helmet>
+  );
+
   return (
     <div className={style.main}>
-      <Helmet>
-        <html lang="en" />
-        <meta charSet="utf-8" />
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>"</title>
-        <meta
-          name=""
-          content=""
-        />
-        <link
-          rel="canonical"
-          href={`https://www.luxurybubblebasket.com/Blog/`}
-        />
-      </Helmet>
       {loading && <p>Loading...</p>}
       {allBlog.map((item) => (
-        <div className={style.container} key={item._id}>
-          <div className={style.blog_img_box}>
-            <img src={item?.blogImage?.url} alt={item?.blogTitle} />
+        <React.Fragment key={item._id}>
+          {generateHelmet(item)}
+          <div className={style.container}>
+            <div className={style.blog_img_box}>
+              <img src={item?.blogImage?.url} alt={item?.blogTitle} />
+            </div>
+            <br />
+            <div className={style.author_box}>
+              <p>{item?.authorName}</p>
+              <p>-</p> <p>{convertDate(item?.createdAt)}</p>
+            </div>
+            <div className={style.title_box}>
+              <h1>{item?.blogTitle}</h1>
+              <p>{item?.shortDescription}</p>
+            </div>
+            <div className={style.bottom_box}>
+              <a
+                href={`/Blog/${formatTitleForUrl(item?.blogTitle)}`}
+                className={style.read_more_link}
+              >
+                READ MORE →
+              </a>
+              <ul>
+                <li>
+                  <FacebookRoundedIcon className={style.icon} />
+                </li>
+                <li>
+                  <InstagramIcon className={style.icon} />
+                </li>
+                <li>
+                  <LinkedInIcon className={style.icon} />
+                </li>
+                <li>
+                  <TwitterIcon className={style.icon} />
+                </li>
+              </ul>
+            </div>
           </div>
-          <br />
-          <div className={style.author_box}>
-            <p>{item?.authorName}</p>
-            <p>-</p> <p>{convertDate(item?.createdAt)}</p>
-          </div>
-          <div className={style.title_box}>
-            <h6>{item?.blogTitle}</h6>
-            <p>{item?.shortDescription}</p>
-          </div>
-          <div className={style.bottom_box}>
-            <a
-              href={`/Blog/${formatTitleForUrl(item?.blogTitle)}`}
-              className={style.read_more_link}
-            >
-              READ MORE →
-            </a>
-            <ul>
-              <li>
-                <FacebookRoundedIcon className={style.icon} />
-              </li>
-              <li>
-                <InstagramIcon className={style.icon} />
-              </li>
-              <li>
-                <LinkedInIcon className={style.icon} />
-              </li>
-              <li>
-                <TwitterIcon className={style.icon} />
-              </li>
-            </ul>
-          </div>
-        </div>
+        </React.Fragment>
       ))}
     </div>
   );
